5ddba2d7e0c55822afdd49326215e686c68fb70b
Frikk Balder <33499052+MindChirp@users.noreply.github.com>
2023-09-11 19:38:05 +0200

    Removed prisma

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
-import { ResponseData } from "@/pages/api/test";
 import { Header } from "../components/Header";
 import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 
 const Home = (
   props: InferGetServerSidePropsType<typeof getServerSideProps>
 ) => {
-  console.log(props, "asdasddadasda");
   return (
     <div
       style={{
@@ -17,27 +15,10 @@ const Home = (
   );
 };
 
-export const getServerSideProps: GetServerSideProps<
-  ResponseData
-> = async () => {
-  let data: ResponseData = {
-    coolString: "",
-    message: "",
-  };
-
-  try {
-    const response = await fetch("http://localhost:3000/api/test", {
-      method: "GET",
-    });
-
-    data = await response.json();
-  } catch (err) {
-    console.error(err);
-  }
-
+export const getServerSideProps = async () => {
   return {
     props: {
-      ...data,
+      users: undefined,
     },
   };
 };
