7ee5bc6c3c300570a9a87134cd25f8bc15aa9e17
Frikk Balder <33499052+MindChirp@users.noreply.github.com>
2023-11-06 16:37:26 +0100

    Basic changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,57 +1,81 @@
-import { getItems } from '@/apis/reservations/reservations';
-import { DetailedItem } from '@/apis/reservations/types';
-import Wave from '@/components/images/Wave';
-import ItemSelectionBox from '@/components/inputs/ItemSelectionBox';
-import { Box, Paper } from '@mui/material';
-import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
+import { Button, Container, Grid, Paper, Typography } from '@mui/material';
+import { Stack } from '@mui/system';
+import dayjs from 'dayjs';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+import { DateTimePicker } from '../components/DateTimePicker';
+import { Kalender } from '../components/kalender/Kalender';
 
-const Home = ({ items }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+const Home = () => {
+  const [fra, setFra] = useState(dayjs());
+  const [til, setTil] = useState(dayjs());
+  const router = useRouter();
+
+  function routeToBooking(type: string) {
+    if (type === 'kontoret') {
+      router.push('/kontoret?start=' + fra.format('HH.mm-DD-MM-YYYY') + '&slutt=' + til.format('HH.mm-DD-MM-YYYY'));
+    } else if (type === 'soundboks') {
+      router.push('/soundboks?start=' + fra.format('HH.mm-DD-MM-YYYY') + '&slutt=' + til.format('HH.mm-DD-MM-YYYY'));
+    }
+  }
   return (
     <>
-      <Box
-        sx={{
-          display: 'flex',
+      <div
+        style={{
           height: '100vh',
-          alignItems: 'center',
-          placeContent: 'center',
-        }}>
-        <ItemSelectionBox items={items} defaultValue='kontoret' />
-      </Box>
-
-      <Paper
-        sx={{
+          overflow: 'hidden',
           position: 'relative',
+          marginTop: '-80px',
         }}>
-        <Wave
+        <Image
+          src={'/kontoret.jpg'}
+          alt=''
+          layout='fill'
           style={{
             position: 'absolute',
-            transform: 'translateY(-100%)',
-            color: 'red',
-            width: '100%',
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            zIndex: -1,
+            filter: 'blur(7.2px)',
+            overflow: 'hidden',
+            boxShadow: 'inset 0 -10px 10px -10px #000000',
           }}
         />
-      </Paper>
+        <Grid container spacing={0} direction='column' alignItems='center' justifyContent='center' sx={{ height: '90vh' }}>
+          <Paper elevation={5} sx={{ p: 5 }}>
+            <Stack spacing={2}>
+              <h1>Reserver</h1>
+              <DateTimePicker title='Fra' value={fra} setValue={setFra} />
+              <DateTimePicker title='Til' value={til} setValue={setTil} />
+              <Stack direction='row' spacing={1}>
+                <Button onClick={() => routeToBooking('kontoret')} sx={{ width: '50%' }} variant='contained' size='large'>
+                  Kontoret
+                </Button>
+                <Button onClick={() => routeToBooking('soundboks')} sx={{ width: '50%' }} variant='contained' size='large'>
+                  Soundboks
+                </Button>
+              </Stack>
+            </Stack>
+          </Paper>
+        </Grid>
+      </div>
+      <Container maxWidth='xl' sx={{ my: 20 }}>
+        <Typography variant='h2'>Kontoret</Typography>
+        <Paper variant='outlined' sx={{ p: 5 }}>
+          <Kalender admin={false} />
+        </Paper>
+      </Container>
+      <Container maxWidth='xl' sx={{ my: 20 }}>
+        <Typography variant='h2'>Soundboks</Typography>
+        <Paper variant='outlined'>
+          <Kalender admin={false} />
+        </Paper>
+      </Container>
     </>
   );
 };
-
-export const getServerSideProps = (async () => {
-  try {
-    const items = (await getItems()).data;
-
-    return {
-      props: {
-        items: items,
-      },
-    };
-  } catch {
-    return {
-      notFound: true,
-    };
-  }
-}) satisfies GetServerSideProps<{
-  items?: DetailedItem[];
-}>;
 
 export default Home;
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,21 @@
+import { signIn } from '@/apis/auth/authentication';
 import { Button, FormControl, Grid, Paper, TextField, Typography } from '@mui/material';
 import { Stack } from '@mui/system';
+import { useMutation } from '@tanstack/react-query';
 
 export default function Page() {
+  const {
+    data,
+    isLoading,
+    mutate: doSignIn,
+  } = useMutation(signIn, {
+    onSuccess: (data) => console.log(data),
+  });
+
+  const handleSubmit = () => {
+    doSignIn({ password: 'index123', username: 'index' });
+  };
+
   return (
     <Grid container spacing={0} direction='column' alignItems='center' justifyContent='center' sx={{ height: '80vh' }}>
       <Paper variant={'outlined'} sx={{ p: 5 }}>
@@ -13,7 +27,7 @@ export default function Page() {
           <FormControl>
             <TextField label='Passord' id='password' type='password' />
           </FormControl>
-          <Button size='large' variant='contained' type='submit'>
+          <Button size='large' variant='contained' type='submit' onClick={handleSubmit}>
             Logg inn
           </Button>
         </Stack>
